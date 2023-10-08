e37bb74b00e211d729dffe97c6d90fe9d177f82f
Jefferson Ding <tyding2004@gmail.com>
2023-10-08 02:53:01 -0400

    update

--- components/meta.tsx
+++ components/meta.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 const Meta = () => (
 	<Head>
-		<title>Rice Bowl</title>
+		<title>Cypress</title>
 		<meta charSet='utf-8' />
 		<meta name='mobile-web-app-capable' content='yes' />
 		<meta name='apple-mobile-web-app-capable' content='yes' />
@@ -10,9 +10,9 @@ const Meta = () => (
 			name='apple-mobile-web-app-status-bar-style'
 			content='black-translucent'
 		/>
-		<meta name='apple-mobile-web-app-title' content='Rice Bowl' />
-		<meta name='application-name' content='Rice Bowl' />
-		<meta name='description' content='Bring your own ingredients' />
+		<meta name='apple-mobile-web-app-title' content='Cypress' />
+		<meta name='application-name' content='Cypress' />
+		<meta name='description' content='Cypress App' />
 		<meta
 			name='theme-color'
 			content='#f4f4f5'
@@ -28,7 +28,7 @@ const Meta = () => (
 			content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
 		/>
 		<link rel='apple-touch-icon' href='/images/icon-maskable-512.png' />
-		<link rel='icon' type='image/png' href='/images/favicon.png' />
+		<link rel='icon' type='image/png' href='/images/favicon.ico' />
 		<link rel='manifest' href='/manifest.json' />
 	</Head>
 )
